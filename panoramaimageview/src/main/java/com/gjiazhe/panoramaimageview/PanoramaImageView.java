aad663e19f6491a81f3e4a89df1230f06ffa15d2
Diogo Almeida <doalmeida94@gmail.com>
2019-02-12 14:04:22 -0100

    final commit

--- panoramaimageview/src/main/java/com/gjiazhe/panoramaimageview/PanoramaImageView.java
+++ panoramaimageview/src/main/java/com/gjiazhe/panoramaimageview/PanoramaImageView.java
@@ -112,11 +112,9 @@ public class PanoramaImageView extends ImageView {
             mDrawableHeight = getDrawable().getIntrinsicHeight();
 
             if (mDrawableWidth * mHeight > mDrawableHeight * mWidth) {
-//                mOrientation = ORIENTATION_HORIZONTAL;
                 float imgScale = (float) mHeight / (float) mDrawableHeight;
                 mMaxOffset = Math.abs((mDrawableWidth * imgScale - mWidth) * 0.5f);
             } else if (mDrawableWidth * mHeight < mDrawableHeight * mWidth) {
-//                mOrientation = ORIENTATION_VERTICAL;
                 float imgScale = (float) mWidth / (float) mDrawableWidth;
                 mMaxOffset = Math.abs((mDrawableHeight * imgScale - mHeight) * 0.5f);
             }
@@ -131,57 +129,27 @@ public class PanoramaImageView extends ImageView {
         }
 
         // Draw image
-//        if (mOrientation == ORIENTATION_HORIZONTAL) {
-            float currentOffsetX = mMaxOffset * mProgress;
-            canvas.save();
-            canvas.translate(currentOffsetX, 0);
-            super.onDraw(canvas);
-            canvas.restore();
-//        } else if (mOrientation == ORIENTATION_VERTICAL) {
-//            float currentOffsetY = mMaxOffset * mProgress;
-//            canvas.save();
-////            canvas.translate(0, currentOffsetY);
-//            canvas.translate(currentOffsetY, 0);
-//            super.onDraw(canvas);
-//            canvas.restore();
-//        }
+        float currentOffsetX = mMaxOffset * mProgress;
+        canvas.save();
+        canvas.translate(currentOffsetX, 0);
+        super.onDraw(canvas);
+        canvas.restore();
 
         // Draw scrollbar
         if (mEnableScrollbar) {
-//            switch (mOrientation) {
-//                case ORIENTATION_HORIZONTAL : { //ORIENTATION_HORIZONTAL: {
-                    float barBgWidth = mWidth * 0.9f;
-                    float barWidth = barBgWidth * mWidth / mDrawableWidth;
+            float barBgWidth = mWidth * 0.9f;
+            float barWidth = barBgWidth * mWidth / mDrawableWidth;
 
-                    float barBgStartX = (mWidth - barBgWidth) / 2;
-                    float barBgEndX = barBgStartX + barBgWidth;
-                    float barStartX = barBgStartX + (barBgWidth - barWidth) / 2 * (1 - mProgress);
-                    float barEndX = barStartX + barWidth;
-                    float barY = mHeight * 0.95f;
+            float barBgStartX = (mWidth - barBgWidth) / 2;
+            float barBgEndX = barBgStartX + barBgWidth;
+            float barStartX = barBgStartX + (barBgWidth - barWidth) / 2 * (1 - mProgress);
+            float barEndX = barStartX + barWidth;
+            float barY = mHeight * 0.95f;
 
-                    mScrollbarPaint.setAlpha(100);
-                    canvas.drawLine(barBgStartX, barY, barBgEndX, barY, mScrollbarPaint);
-                    mScrollbarPaint.setAlpha(255);
-                    canvas.drawLine(barStartX, barY, barEndX, barY, mScrollbarPaint);
-//                    break;
-//                }
-//                case ORIENTATION_VERTICAL: {//ORIENTATION_VERTICAL: {
-//                    float barBgHeight = mHeight * 0.9f;
-//                    float barHeight = barBgHeight * mHeight / mDrawableHeight;
-//
-//                    float barBgStartY = (mHeight - barBgHeight) / 2;
-//                    float barBgEndY = barBgStartY + barBgHeight;
-//                    float barStartY = barBgStartY + (barBgHeight - barHeight) / 2 * (1 - mProgress);
-//                    float barEndY = barStartY + barHeight;
-//                    float barX = mWidth * 0.95f;
-//
-//                    mScrollbarPaint.setAlpha(100);
-//                    canvas.drawLine(barX, barBgStartY, barX, barBgEndY, mScrollbarPaint);
-//                    mScrollbarPaint.setAlpha(255);
-//                    canvas.drawLine(barX, barStartY, barX, barEndY, mScrollbarPaint);
-//                    break;
-//                }
-//            }
+            mScrollbarPaint.setAlpha(100);
+            canvas.drawLine(barBgStartX, barY, barBgEndX, barY, mScrollbarPaint);
+            mScrollbarPaint.setAlpha(255);
+            canvas.drawLine(barStartX, barY, barEndX, barY, mScrollbarPaint);
         }
     }
 

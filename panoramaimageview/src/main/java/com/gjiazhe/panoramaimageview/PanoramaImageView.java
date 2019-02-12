88ea2ee4790287fcac7158285851fdbf98568ed6
Diogo Almeida <doalmeida94@gmail.com>
2019-02-12 11:49:44 -0100

    edit image & scrollbar draw

--- panoramaimageview/src/main/java/com/gjiazhe/panoramaimageview/PanoramaImageView.java
+++ panoramaimageview/src/main/java/com/gjiazhe/panoramaimageview/PanoramaImageView.java
@@ -131,25 +131,25 @@ public class PanoramaImageView extends ImageView {
         }
 
         // Draw image
-        if (mOrientation == ORIENTATION_HORIZONTAL) {
+//        if (mOrientation == ORIENTATION_HORIZONTAL) {
             float currentOffsetX = mMaxOffset * mProgress;
             canvas.save();
             canvas.translate(currentOffsetX, 0);
             super.onDraw(canvas);
             canvas.restore();
-        } else if (mOrientation == ORIENTATION_VERTICAL) {
-            float currentOffsetY = mMaxOffset * mProgress;
-            canvas.save();
-//            canvas.translate(0, currentOffsetY);
-            canvas.translate(currentOffsetY, 0);
-            super.onDraw(canvas);
-            canvas.restore();
-        }
+//        } else if (mOrientation == ORIENTATION_VERTICAL) {
+//            float currentOffsetY = mMaxOffset * mProgress;
+//            canvas.save();
+////            canvas.translate(0, currentOffsetY);
+//            canvas.translate(currentOffsetY, 0);
+//            super.onDraw(canvas);
+//            canvas.restore();
+//        }
 
         // Draw scrollbar
         if (mEnableScrollbar) {
-            switch (mOrientation) {
-                case ORIENTATION_HORIZONTAL : { //ORIENTATION_HORIZONTAL: {
+//            switch (mOrientation) {
+//                case ORIENTATION_HORIZONTAL : { //ORIENTATION_HORIZONTAL: {
                     float barBgWidth = mWidth * 0.9f;
                     float barWidth = barBgWidth * mWidth / mDrawableWidth;
 
@@ -163,25 +163,25 @@ public class PanoramaImageView extends ImageView {
                     canvas.drawLine(barBgStartX, barY, barBgEndX, barY, mScrollbarPaint);
                     mScrollbarPaint.setAlpha(255);
                     canvas.drawLine(barStartX, barY, barEndX, barY, mScrollbarPaint);
-                    break;
-                }
-                case ORIENTATION_VERTICAL: {//ORIENTATION_VERTICAL: {
-                    float barBgHeight = mHeight * 0.9f;
-                    float barHeight = barBgHeight * mHeight / mDrawableHeight;
-
-                    float barBgStartY = (mHeight - barBgHeight) / 2;
-                    float barBgEndY = barBgStartY + barBgHeight;
-                    float barStartY = barBgStartY + (barBgHeight - barHeight) / 2 * (1 - mProgress);
-                    float barEndY = barStartY + barHeight;
-                    float barX = mWidth * 0.95f;
-
-                    mScrollbarPaint.setAlpha(100);
-                    canvas.drawLine(barX, barBgStartY, barX, barBgEndY, mScrollbarPaint);
-                    mScrollbarPaint.setAlpha(255);
-                    canvas.drawLine(barX, barStartY, barX, barEndY, mScrollbarPaint);
-                    break;
-                }
-            }
+//                    break;
+//                }
+//                case ORIENTATION_VERTICAL: {//ORIENTATION_VERTICAL: {
+//                    float barBgHeight = mHeight * 0.9f;
+//                    float barHeight = barBgHeight * mHeight / mDrawableHeight;
+//
+//                    float barBgStartY = (mHeight - barBgHeight) / 2;
+//                    float barBgEndY = barBgStartY + barBgHeight;
+//                    float barStartY = barBgStartY + (barBgHeight - barHeight) / 2 * (1 - mProgress);
+//                    float barEndY = barStartY + barHeight;
+//                    float barX = mWidth * 0.95f;
+//
+//                    mScrollbarPaint.setAlpha(100);
+//                    canvas.drawLine(barX, barBgStartY, barX, barBgEndY, mScrollbarPaint);
+//                    mScrollbarPaint.setAlpha(255);
+//                    canvas.drawLine(barX, barStartY, barX, barEndY, mScrollbarPaint);
+//                    break;
+//                }
+//            }
         }
     }
 
